7e413ba4f80e7b80aba478a7e33ec03c6619e5ca
david <david.siarhei@gmail.com>
2024-12-06 17:28:07 +0300

    # displaying lists and detailed information

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -28,7 +28,7 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file.
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    summary = models.TextField(max_length=2000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
@@ -110,3 +110,7 @@ class Author(models.Model):
         String for representing the Model object.
         """
         return '%s, %s' % (self.last_name, self.first_name)
+
+
+    class Meta:
+        ordering = ['last_name']
